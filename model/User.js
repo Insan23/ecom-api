f3e12bcc21f02e082cad4536c68382e150a21118
Insan23 <12586149+Insan23@users.noreply.github.com>
2025-10-30 22:11:05 +0700

    nambah fitur

--- model/User.js
+++ model/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+//oaasldmasd
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
